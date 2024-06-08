b5a7e8244502c8f8be1cdd5d842d8ddbe78718bf
Dadacx <84072588+Dadacx@users.noreply.github.com>
2024-06-08 14:12:04 +0200

    Updates

---
+++ static/js/952.0ca2a990.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkt=self.webpackChunkt||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.0ca2a990.chunk.js.map
